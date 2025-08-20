3b95e2abc3a91108cff7082d08306618252ffea8
JainamNalwaya <139463378+JainamNalwaya@users.noreply.github.com>
2025-08-20 10:40:02 +0530

    jainam1

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ document.getElementById("btn").addEventListener("click", function() { //swati
   document.getElementById("message").textContent = "You clicked the button!";  //JAINAM
 });
 // github 
+
+
+// jainam1 Chnages
